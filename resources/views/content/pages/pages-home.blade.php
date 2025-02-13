ea511b4a919c1472b48a7070c0430b91c4c1445f
Ahmed Mahmoud <Ahmed Mahmoud@AHMED_MAHMOUD>
2025-02-13 15:54:20 +0200

    pharmacy

--- resources/views/content/pages/pages-home.blade.php
+++ resources/views/content/pages/pages-home.blade.php
@@ -9,169 +9,51 @@
 @section('content')
 <!-- Examples -->
 <div class="row mb-12 g-6">
-  <div class="col-md-6 col-lg-4">
+  <!-- Make Appointment Card -->
+  <div class="col-md-6 col-lg-6">
     <div class="card h-100">
       <img class="card-img-top" src="{{url('https://th.bing.com/th/id/OIP.B1TorMhyNFXBDUW-1A87qQHaE8?rs=1&pid=ImgDetMain')}}" alt="Card image" />
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Make Appointment</h5>
         <p class="card-text">
           Some quick example text to build on the card title and make up the bulk of the card's content.
         </p>
-        <a href="javascript:void(0)" class="btn btn-outline-primary">Make Appoinment</a>
+        <a href="javascript:void(0)" class="btn btn-outline-primary">Make Appointment</a>
       </div>
     </div>
   </div>
-  <div class="col-md-6 col-lg-4">
-    <div class="card h-100">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle">Support card subtitle</h6>
-      </div>
-      <img class="img-fluid" src="{{asset('assets/img/elements/10.jpg')}}" alt="Card image cap" />
-      <div class="card-body">
-        <p class="card-text">Bear claw sesame snaps gummies chocolate.</p>
-        <a href="javascript:void(0);" class="card-link">Card link</a>
-        <a href="javascript:void(0);" class="card-link">Another link</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-4">
-    <div class="card h-100">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle">Support card subtitle</h6>
-        <img class="img-fluid d-flex mx-auto my-6 rounded" src="{{asset('assets/img/elements/4.jpg')}}" alt="Card image cap" />
-        <p class="card-text">Bear claw sesame snaps gummies chocolate.</p>
-        <a href="javascript:void(0);" class="card-link">Card link</a>
-        <a href="javascript:void(0);" class="card-link">Another link</a>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Examples -->
 
-<!-- Content types
-<h5 class="pb-1 mb-6">Content types</h5>
+  <!-- Pharmacy Card -->
+  <div class="col-md-6 col-lg-6">
+    <div class="card text-white p-4" style="
+      background-color: #1E73BE;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      height: 150px;
+      border-radius: 10px;
+    ">
+      <!-- Left Side -->
+      <div>
+        <h5 class="fw-bold">Pharmacy</h5>
+        <p>Get your medicine and all your pharmacy needs.</p>
+        <a href="javascript:void(0)" class="btn btn-outline-light px-4 py-2">Place order</a>
+      </div>
 
-<div class="row mb-12 g-6">
-  <div class="col-md-6 col-lg-4">
-    <h6 class="mt-2 text-muted">Body</h6>
-    <div class="card mb-6">
-      <div class="card-body">
-        <p class="card-text">
-          This is some text within a card body.
-          Jelly lemon drops tiramisu chocolate cake cotton candy soufflé oat cake sweet roll. Sugar plum marzipan dragée
-          topping cheesecake chocolate bar. Danish muffin icing donut.
-        </p>
-      </div>
-    </div>
-    <h6 class="mt-2 text-muted">Titles, text, and links</h6>
-    <div class="card mb-6">
-      <div class="card-body">
-        <h5 class="card-title mb-1">Card title</h5>
-        <div class="card-subtitle mb-4">Card subtitle</div>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-        <a href="javascript:void(0)" class="card-link">Card link</a>
-        <a href="javascript:void(0)" class="card-link">Another link</a>
-      </div>
-    </div>
-    <h6 class="mt-2 text-muted">List groups</h6>
-    <div class="card mb-6">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-4">
-    <h6 class="mt-2 text-muted">Images</h6>
-    <div class="card">
-      <img class="card-img-top" src="{{asset('assets/img/elements/5.jpg')}}" alt="Card image cap" />
-      <div class="card-body">
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-        <p class="card-text">Cookie topping caramels jujubes gingerbread. Lollipop apple pie cupcake candy canes cookie
-          ice cream. Wafer chocolate bar carrot cake jelly-o.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-4">
-    <h6 class="mt-2 text-muted">Kitchen sink</h6>
-    <div class="card">
-      <img class="card-img-top" src="{{asset('assets/img/elements/7.jpg')}}" alt="Card image cap" />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title.
-        </p>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
-      <div class="card-body">
-        <a href="javascript:void(0)" class="card-link">Card link</a>
-        <a href="javascript:void(0)" class="card-link">Another link</a>
+      <!-- Right Side Image -->
+      <div>
+        <img src="your-image-url.png" alt="Pharmacy Items" style="height: 100px;">
       </div>
     </div>
   </div>
 </div>
 
-<h6 class="pb-1 mb-6 text-muted">Header and footer</h6>
-<div class="row mb-12 g-6">
-  <div class="col-md-6 col-lg-4">
-    <div class="card">
-      <div class="card-header">
-        Featured
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">
-          With supporting text below as a natural lead-in to additional content natural lead-in to additional content.
-        </p>
-        <a href="javascript:void(0)" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-4">
-    <div class="card">
-      <h5 class="card-header">Quote</h5>
-      <div class="card-body">
-        <blockquote class="blockquote mb-0">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit
-            amet,
-            consectetur.
-          </p>
-          <footer class="blockquote-footer">
-            Someone famous in
-            <cite title="Source Title">Source Title</cite>
-          </footer>
-        </blockquote>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-4">
-    <div class="card text-center">
-      <div class="card-header">
-        Featured
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural.</p>
-        <a href="javascript:void(0)" class="btn btn-primary">Go somewhere</a>
-      </div>
-      <div class="card-footer text-muted">
-        2 days ago
-      </div>
-    </div>
-  </div>
-</div>
-/ Content types -->
+
+
+
+
+
 <div class="col-md">
   <div class="card shadow-none bg-success-">
     <div class="card-body text-success">
@@ -309,17 +191,7 @@
 
 
 <!-- Text alignment -->
-<h5 class="pb-1 mb-6">Text alignment</h5>
-<div class="row mb-12 g-6">
-  <div class="col-md-6 col-lg-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="javascript:void(0)" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
+
   <div class="col-md-6 col-lg-4">
     <div class="card text-center">
       <div class="card-body">
